20862114d13789016bda5271dfacece4172623fe
Shoirhi <shoirhi.k@gmail.com>
2025-10-29 01:15:55 +0900

    Configure Astro for Cloudflare worker build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fortnite-result-viewer.plain-star-15af.workers.dev",
+  output: "server",
+  adapter: cloudflare(),
 
   vite: {
     plugins: [tailwindcss()]
